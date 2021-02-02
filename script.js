616787dba701f3bd0b3584765f427b57b972738f
Higo Sampaio <hglbsm@gmail.com>
2021-02-02 11:13:18 -0300

    feat: Set more photos with scroll limit

--- script.js
+++ script.js
@@ -1,36 +1,88 @@
-const imageContainer = document.querySelector('#image-container');
-const loader = document.querySelector('#loader');
+const imageContainer = document.querySelector("#image-container");
+const loader = document.querySelector("#loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+let isInitialLoad = true;
 
-const count = 10;
-const apiKey = 'XE8hzFnJKoROP0ZjOoqEP81BgkaDOY3sbQJ9mWCII68';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+// Unsplash API
+let initialCount = 5;
+const apiKey = "XE8hzFnJKoROP0ZjOoqEP81BgkaDOY3sbQJ9mWCII68";
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${initialCount}`;
+
+function updatedAPIURLWithNewCount(picCount) {
+  apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${picCount}`;
+}
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+function setElementAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
 
 function displayPhotos() {
-  photosArray.forEach(photo => {
-    const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
 
-    const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    setElementAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    const img = document.createElement("img");
+    setElementAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
 
+    // Check when each is finished loading
+    img.addEventListener("load", imageLoaded);
+
+    // Add images to container
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
 }
 
+// Get photos from Unsplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
     displayPhotos();
-  } catch(error) {
-    console.error('Woops!', error);
+    if (isInitialLoad) {
+      updatedAPIURLWithNewCount(10);
+      isInitialLoad = false;
+    }
+  } catch (error) {
+    console.error("Woops!", error);
   }
 }
 
+// Set more photos with scroll limit
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+// On Load
 getPhotos();
